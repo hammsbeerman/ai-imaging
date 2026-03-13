afc5d6de0dfa4810b96eaccc2f21cecc39a95435
Adam Kaney <adam.kaney@gmail.com>
2026-03-13 21:07:02 +0000

    update celery

--- media_index/celery.py
+++ media_index/celery.py
@@ -5,4 +5,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "media_index.settings")
 
 app = Celery("media_index")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+app.autodiscover_tasks(
+    [
+        "indexer",
+    ]
+)
+
+existing_imports = tuple(getattr(app.conf, "imports", ()) or ())
+app.conf.imports = existing_imports + (
+    "indexer.tasks",
+    "indexer.tasks_preview",
+    "indexer.tasks_metadata",
+    "indexer.tasks_dedupe",
+)
